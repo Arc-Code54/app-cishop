8eb0bda2ee1f7ea7af9fb22d8b433b7b8f97c48d
Saeful Ardi <saeful.4rdi@gmail.com>
2026-05-19 02:52:28 +0700

    update

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -8,8 +8,11 @@ class Register extends MY_Controller
 	public function __construct()
 	{
 		parent::__construct();
+
+		//Mengambil data session bernama is_login.
 		$is_login = $this->session->userdata('is_login');
 
+		// Mencegah user yang sudah login membuka halaman register dan di arahkan kehalaman utama		
 		if ($is_login) {
 			redirect(base_url());
 			return;
@@ -18,27 +21,36 @@ class Register extends MY_Controller
 
 	public function index()
 	{
+		// Mengecek apakah ada data form yang dikirim.
 		if (!$_POST) {
+			// Mengambil nilai default dari model register.
 			$input = (object) $this->register->getDefaultValues();
 		} else {
+			// engambil semua data POST
 			$input = (object) $this->input->post(null, true);
 		}
 
+		// Menjalankan validasi form.
 		if (!$this->register->validate()) {
-			$data['title'] 	= 'Register';
-			$data['input'] 	= $input;
-			$data['page']	= 'pages/auth/register';
-			$this->view($data);
-			return;
+			$data['title'] 	= 'Register';				// Judul Halaman
+			$data['input'] 	= $input;					// Data Form
+			$data['page']	= 'pages/auth/register';	// File View
+			$this->view($data);							// Menampilkan halaman register.
+			return;										// Menghentikan eksekusi setelah view tampil
 		}
 
+		// Menjalankan proses penyimpanan user ke database
 		if ($this->register->run($input)) {
+			// Menyimpan pesan sementara
 			$this->session->set_flashdata('success', 'Berhasil melakukan registrasi!');
+			// Mengarahkan user ke homepage.
 			redirect(base_url());
 		} else {
+			// Menampilkan pesan error.
 			$this->session->set_flashdata('error', 'Oops! terjadi suatu kesalahan!');
+			// Kembali ke halaman register
 			redirect(base_url('/register'));
-			}
+		}
 	}
 
 }
